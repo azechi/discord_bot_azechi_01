4f2a17afc94f2a705550ceceb1dd1caaa3680f60
azechi <azechi.n@gmail.com>
2020-04-09 10:46:52 +0900

    .

--- app/main.rb
+++ app/main.rb
@@ -4,5 +4,13 @@ bot = Discordrb::Bot.new token: ENV["TOKEN"]
 bot.message(with_text: 'Ping!') do |event|
 	event.respond 'Pong!'
 end
+
+require 'thread'
+mutex = Mutex.new
+Signal.trap(:INT) do
+	Thread.new { mutex.synchronize { bot.stop } }
+end
+#Signal.trap(:TERM) { bot.stop }
+
 bot.run
 
