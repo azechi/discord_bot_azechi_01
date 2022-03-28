3ac775ab85e91fdf73e1ae420959d7b2c533b221
azechi <azechi.n@gmail.com>
2022-03-28 11:41:10 +0900

    .

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::env;
+
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("BOT_TOKEN").expect("token");
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    let shard_man = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Could not register ctrl+c handler");
+        shard_man.lock().await.shutdown_all().await;
+    });
+
+    match client.start().await {
+        Err(why) => println!("Client error: {:?}", why),
+        s => println!("Client shutdown: {:?}", s),
+    }
+}
