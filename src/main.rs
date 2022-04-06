23c197bfb2ac8741e78ca41ccbc298c8d28b209b
azechi <azechi@users.noreply.github.com>
2022-04-06 10:48:51 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ struct Handler;
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!!").await {
                 println!("Error sending message: {:?}", why);
             }
         }
